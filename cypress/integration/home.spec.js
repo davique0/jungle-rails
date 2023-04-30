7f96625bd24f95d049ee83845fb88ecb857a1c30
davique0 <davique@gmail.com>
2023-04-30 19:15:35 -0400

    Test products in homepage

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,4 +2,8 @@ describe("jungle-rails app", () => {
   it("displays home page", () => {
     cy.visit("/");
   });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
 });
