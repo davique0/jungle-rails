0fff2ab3f5c91dbdc9a2e50b8d789614037bbc98
davique0 <davique@gmail.com>
2023-04-30 19:17:51 -0400

    Test with number of products in homepage

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -6,4 +6,8 @@ describe("jungle-rails app", () => {
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
